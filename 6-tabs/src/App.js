6f3002012cfee48cedac46cda30cb641dd964ee1
mohammedahw <mohammedalihw@gmail.com>
2021-10-18 11:47:02 +0300

    finished tabs

--- 6-tabs/src/App.js
+++ 6-tabs/src/App.js
@@ -3,7 +3,69 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 
 const url = "https://course-api.com/react-tabs-project";
 function App() {
-  return <h2>tabs project setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(1);
+
+  const fetchJobs = async () => {
+    const respone = await fetch(url);
+    const newJob = await respone.json();
+    setJobs(newJob);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading ...</h1>
+      </section>
+    );
+  }
+
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>expierence</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        {}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="jov-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App;
